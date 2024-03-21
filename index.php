02f8d6e81afdb9c4db4e7ecfc6ecf35b6484562c
Samuel MUHIZI <muhizisam5@gmail.com>
2024-03-21 11:50:03 +0200

    Dockerization is completed

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
 echo "DEVOPS EXAMINATIONS PRACTICE";
 
 ?>
-<a href="register.php">registration</a><br>
+<a href="registration.php">registration</a><br>
 <a href="login.php">log in</a>
--- login.php
+++ login.php
@@ -1,23 +1,23 @@
 <form action="action_page.php" method="post">
   <div class="imgcontainer">
-    <img src="img_avatar2.png" alt="Avatar" class="avatar">
+    
   </div>
 
   <div class="container">
     <label for="uname"><b>Email</b></label>
-    <input type="text" placeholder="Enter Username" name="mail" required>
+    <input type="text" placeholder="Enter Username" name="mail" required><br>
 
     <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
+    <input type="password" placeholder="Enter Password" name="psw" required><br>
 
-    <button type="submit">Login</button>
+    <button type="submit">Login</button><br>
     <label>
-      <input type="checkbox" checked="checked" name="remember"> Remember me
+      <input type="checkbox" checked="checked" name="remember"> 
     </label>
   </div>
 
   <div class="container" style="background-color:#f1f1f1">
-    <button type="button" class="cancelbtn">Cancel</button>
+    
     <span class="psw">Forgot <a href="registration.php">password?</a></span>
   </div>
 </form>
