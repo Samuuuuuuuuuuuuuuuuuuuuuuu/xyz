a05c058a531ad0bbfdcb2c8ef16a9f904456ea13
Samuel MUHIZI <muhizisam5@gmail.com>
2024-03-21 11:23:43 +0200

    Login page is created

---
+++ index.php
@@ -0,0 +1,7 @@
+<php
+
+echo "DEVOPS EXAMINATIONS PRACTICE";
+
+?>
+<a href="register.php">registration</a><br>
+<a href="login.php">log in</a>
---
+++ registration.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html> <html> <head> 	<title> Form to Register</title>
+</head>  <body>  <table> <form action="" method="POST">
+<tr><td>First Name</td><td><input type="text" name="fname" ></td></tr>
+<tr><td>Last name</td><td><input type="text" name="regnum"></td></tr>
+<tr><td>Gender</td><td><input type="text" name="regnum"></td></tr>
+<tr><td>Email</td><td><input type="text" name="regnum"></td></tr>
+<tr><td>password</td><td><input type="text" name="regnum"></td></tr>
+<tr><td><input type="submit" name="submit"></td>
+<td><a  href="index.php">View</a></td></tr>
+</form> </table></body> </html>
